8f3bcfc03b416657d337f0687ad14231a303a0b9
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-06-14 12:21:38 +0200

    Reformat Location Tracker

--- app/src/main/java/pl/patrykkotlin/myweather/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/pl/patrykkotlin/myweather/data/location/DefaultLocationTracker.kt
@@ -1,6 +1,7 @@
 package pl.patrykkotlin.myweather.data.location
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
@@ -8,6 +9,9 @@ import android.location.Location
 import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import kotlinx.coroutines.suspendCancellableCoroutine
 import pl.patrykkotlin.myweather.domain.location.LocationTracker
 import javax.inject.Inject
@@ -18,46 +22,53 @@ class DefaultLocationTracker @Inject constructor(
     private val application: Application
 ) : LocationTracker {
 
+    @SuppressLint("MissingPermission")
     override suspend fun getCurrentLocation(): Location? {
-        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
-            application,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+        val locationPermissionGranted = checkLocationPermission()
+        val isGpsEnabled = checkGpsEnabled()
 
-        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
-            application,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        val locationManager =
-            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
+        if (!locationPermissionGranted || !isGpsEnabled) {
             return null
         }
 
+        val locationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setNumUpdates(1)
+
         return suspendCancellableCoroutine { cont ->
-            locationClient.lastLocation.apply {
-                if (isComplete) {
-                    if (isSuccessful) {
-                        cont.resume(result)
-                    } else {
-                        cont.resume(null)
-                    }
-                    return@suspendCancellableCoroutine
-                }
-                addOnSuccessListener {
-                    cont.resume(it)
-                }
-                addOnFailureListener {
-                    cont.resume(null)
-                }
-                addOnCanceledListener {
-                    cont.cancel()
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    val location = locationResult.lastLocation
+                    cont.resume(location)
+                    locationClient.removeLocationUpdates(this)
                 }
             }
+
+            locationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                null
+            )
+
+            cont.invokeOnCancellation {
+                locationClient.removeLocationUpdates(locationCallback)
+            }
         }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        val fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+        val coarseLocationPermission = Manifest.permission.ACCESS_COARSE_LOCATION
+
+        return (ContextCompat.checkSelfPermission(application, fineLocationPermission)
+                == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(application, coarseLocationPermission)
+                        == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun checkGpsEnabled(): Boolean {
+        val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 }
